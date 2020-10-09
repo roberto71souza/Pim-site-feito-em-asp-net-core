cb9302cba6c8d0f05e58b8550335fb2c2e0d55ce
roberto souza <roberto71souza@gmail.com>
2020-10-09 17:18:37 -0300

    Verificacoes de campos, tratamento de erros completos

--- ContabilyContextWebSite/Controllers/HomeController.cs
+++ ContabilyContextWebSite/Controllers/HomeController.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using ContabilyContextWebSite.Models;
-using System.Net.Mail;
-using ContabilyContextWebSite.Extension;
 
 namespace ContabilyContextWebSite.Controllers
 {
@@ -19,35 +13,23 @@ namespace ContabilyContextWebSite.Controllers
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Index(Email email)
         {
-            string _email = email._Email;
-            string subject = $"Contato de {email.Nome} para informacoes de servico";
-            string mensagem = $"Nome: {email.Nome} \nTelefone: {email.Telefone} \nEmail:{_email} \nMensagem: {email.Mensagem}";
-
-            if (email.CamposValidos())
+            try
             {
-                using (MailMessage mm = new MailMessage())
+                if (await Email.Enviar(email))
                 {
-                    mm.From = new MailAddress("contabilycontext@gmail.com");
-                    mm.To.Add("contabilycontext@gmail.com");
-                    mm.Subject = subject;
-                    mm.Body = mensagem;
-                    mm.IsBodyHtml = false;
-/*                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
-                    {
-                        smtp.Credentials = new System.Net.NetworkCredential("contabilycontext@gmail.com", "context1968");
-                        smtp.EnableSsl = true;
-                        await smtp.SendMailAsync(mm);
-                    }*/
+                    TempData["Message"] = $"{email.Nome} seu email foi enviado com sucesso!!";
                 }
-                TempData["Message"] = $"{email.Nome} seu email foi enviado com sucesso!!";
-                return View("Index", email);
+                else
+                {
+                    TempData["Message"] = "Os campos obrigatorios nao foram preenchidos";
+                    TempData["CampoInvalido"] = true;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                TempData["Message"] = "Os campos obrigatorios nao foram preenchidos";
+                TempData["Message"] = $"erro ao enviar Email:\n {ex}";
                 TempData["CampoInvalido"] = true;
             }
             return View("Index", email);
--- ContabilyContextWebSite/Models/Email.cs
+++ ContabilyContextWebSite/Models/Email.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContabilyContextWebSite.Extension;
+using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace ContabilyContextWebSite.Models
@@ -12,5 +12,35 @@ namespace ContabilyContextWebSite.Models
         public string Telefone { get; set; }
         public string _Email { get; set; }
         public string Mensagem { get; set; }
+
+        public static async Task<bool> Enviar(Email email)
+        {
+            string _email = email._Email;
+            string subject = $"Contato de {email.Nome} para informacoes de servico";
+            string mensagem = $"Nome: {email.Nome} \nTelefone: {email.Telefone} \nEmail:{_email} \nMensagem: {email.Mensagem}";
+
+            if (email.CamposValidos())
+            {
+                using (MailMessage mm = new MailMessage())
+                {
+                    mm.From = new MailAddress("contabilycontext@gmail.com");
+                    mm.To.Add("contabilycontext@gmail.com");
+                    mm.Subject = subject;
+                    mm.Body = mensagem;
+                    mm.IsBodyHtml = false;
+                //   using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                //     {
+                //         smtp.Credentials = new System.Net.NetworkCredential("contabilycontext@gmail.com", "context1968");
+                //         smtp.EnableSsl = true;
+                //         await smtp.SendMailAsync(mm);
+                //     }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
